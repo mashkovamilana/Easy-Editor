31462282875320711ff2e3ca4606a4e3e9f08676
Viktor Mashkov <viktor.mashkov@quantori.com>
2025-02-08 12:05:51 +0500

    add image functionality

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
+import os
+from PIL import Image, ImageQt
 from PyQt6.QtCore import Qt
-from PyQt6.QtWidgets import QApplication, QWidget, QLabel, QPushButton, QGroupBox, QVBoxLayout, QHBoxLayout, QTextEdit, QMessageBox, QListWidget
+from PyQt6.QtGui import QPixmap
+from PyQt6.QtWidgets import QApplication, QWidget, QLabel, QPushButton, QGroupBox, QVBoxLayout, QHBoxLayout, QTextEdit, \
+    QMessageBox, QListWidget, QFileDialog
+
+workdir = ' '
+
 app = QApplication([])
 main_win = QWidget()
 main_win.setFixedSize(800, 600)
@@ -32,6 +39,60 @@ layout_main.addLayout(layout2)
 layout4.addLayout(layout3)
 layout_main.addLayout(layout4)
 main_win.setLayout(layout_main)
+
+
+class Image_processor():
+
+    def __init__(self):
+        self.filename = None
+        self.image = None
+        self.pixmap = None
+        self.image_path = None
+
+    def load_image(self, filename):
+        self.filename = filename
+        self.image_path = os.path.join(workdir, filename)
+        self.image = Image.open(self.image_path)
+        #self.pixmap = QPixmap(self.image_path)
+        image_qt = ImageQt.ImageQt(self.image)
+        self.pixmap = QPixmap.fromImage(image_qt)
+        width, height = picture.width(), picture.height()
+        self.pixmap = self.pixmap.scaled(width, height, Qt.AspectRatioMode.KeepAspectRatio)
+
+    def show_image(self):
+        picture.setPixmap(self.pixmap)
+
+image_processor = Image_processor()
+
+def open_workdir():
+    global workdir
+    workdir = QFileDialog.getExistingDirectory()
+
+
+def filter(files, extensions):
+    result = []
+    for filename in files:
+        for extension in extensions:
+            if filename.endswith(extension):
+                result.append(filename)
+    return result
+
+
+def show_filenames_list():
+    open_workdir()
+    files = os.listdir(workdir)
+    extensions = ['.jpg', '.jpeg', '.png', '.gif']
+    files = filter(files, extensions)
+    picture_list_widget.clear()
+    picture_list_widget.addItems(files)
+
+def show_picture():
+    filename = picture_list_widget.selectedItems()[0].text()
+    image_processor.load_image(filename)
+    image_processor.show_image()
+
+picture_list_widget.itemClicked.connect(show_picture)
+folder_button.clicked.connect(show_filenames_list)
 
 main_win.show()
 app.exec()
